a9a204b31e68ab9e80c939cec7f0f3ae15e58e8b
Jure Bajic <jure.bajic@memgraph.io>
2021-10-04 18:18:02 +0200

    Enable temporal tests only on 2.0

--- test/test_glue.py
+++ test/test_glue.py
@@ -15,19 +15,19 @@
 # limitations under the License.
 
 
+import datetime
 import sys
-from common import start_memgraph, Memgraph
+
 import mgclient
 import pytest
-import datetime
+
+from common import Memgraph, start_memgraph
 
 
 @pytest.fixture(scope="function")
 def memgraph_connection():
     memgraph = start_memgraph()
-    conn = mgclient.connect(
-        host=memgraph.host, port=memgraph.port, sslmode=memgraph.sslmode()
-    )
+    conn = mgclient.connect(host=memgraph.host, port=memgraph.port, sslmode=memgraph.sslmode())
     conn.autocommit = True
     yield conn
 
@@ -90,9 +90,7 @@ def test_string(memgraph_connection):
     cursor.execute("RETURN '\u010C\u017D\u0160'")
     assert cursor.fetchall() == [("ČŽŠ",)]
 
-    cursor.execute(
-        "RETURN $value", {"value": b"\x01\x0C\x01\x7D\x01\x60".decode("utf-16be")}
-    )
+    cursor.execute("RETURN $value", {"value": b"\x01\x0C\x01\x7D\x01\x60".decode("utf-16be")})
     assert cursor.fetchall() == [("ČŽŠ",)]
 
 
@@ -134,9 +132,7 @@ def test_map(memgraph_connection):
     )
 
     result = cursor.fetchall()
-    assert result == [
-        ({"x": 1, "y": 2, "map": {key_in_a_map: "value"}, "list": value_in_a_map},)
-    ]
+    assert result == [({"x": 1, "y": 2, "map": {key_in_a_map: "value"}, "list": value_in_a_map},)]
 
     value_in_a_map_from_result = result[0][0]["list"]
     # This checks if the reference number of the values in a mg_map are
@@ -160,17 +156,13 @@ def test_map(memgraph_connection):
 def test_node(memgraph_connection):
     conn = memgraph_connection
     cursor = conn.cursor()
-    cursor.execute(
-        "CREATE (n:Label1:Label2 {prop1 : 1, prop2 : 'prop2'}) " "RETURN id(n), n"
-    )
+    cursor.execute("CREATE (n:Label1:Label2 {prop1 : 1, prop2 : 'prop2'}) " "RETURN id(n), n")
     rows = cursor.fetchall()
     node_id, _ = rows[0]
     assert rows == [
         (
             node_id,
-            mgclient.Node(
-                node_id, set(["Label1", "Label2"]), {"prop1": 1, "prop2": "prop2"}
-            ),
+            mgclient.Node(node_id, set(["Label1", "Label2"]), {"prop1": 1, "prop2": "prop2"}),
         )
     ]
 
@@ -181,10 +173,7 @@ def test_node(memgraph_connection):
 def test_relationship(memgraph_connection):
     conn = memgraph_connection
     cursor = conn.cursor()
-    cursor.execute(
-        "CREATE (n)-[e:Type {prop1 : 1, prop2 : 'prop2'}]->(m) "
-        "RETURN id(n), id(m), id(e), e"
-    )
+    cursor.execute("CREATE (n)-[e:Type {prop1 : 1, prop2 : 'prop2'}]->(m) " "RETURN id(n), id(m), id(e), e")
     rows = cursor.fetchall()
     start_id, end_id, edge_id, _ = rows[0]
     assert rows == [
@@ -192,9 +181,7 @@ def test_relationship(memgraph_connection):
             start_id,
             end_id,
             edge_id,
-            mgclient.Relationship(
-                edge_id, start_id, end_id, "Type", {"prop1": 1, "prop2": "prop2"}
-            ),
+            mgclient.Relationship(edge_id, start_id, end_id, "Type", {"prop1": 1, "prop2": "prop2"}),
         )
     ]
 
@@ -217,9 +204,7 @@ def test_path(memgraph_connection):
     e1 = mgclient.Relationship(e1_id, n2_id, n1_id, "Edge1", {})
     e2 = mgclient.Relationship(e2_id, n2_id, n3_id, "Edge2", {})
 
-    assert rows == [
-        (n1_id, n2_id, n3_id, e1_id, e2_id, mgclient.Path([n1, n2, n3], [e1, e2]))
-    ]
+    assert rows == [(n1_id, n2_id, n3_id, e1_id, e2_id, mgclient.Path([n1, n2, n3], [e1, e2]))]
 
     with pytest.raises(ValueError):
         cursor.execute("RETURN $path", {"path": rows[0][5]})
@@ -243,6 +228,8 @@ def test_tuple(memgraph_connection):
         #    count
         assert sys.getrefcount(value_in_tuple) == 3
 
+
+@pytest.mark.temporal
 def test_time(memgraph_connection):
     conn = memgraph_connection
     cursor = conn.cursor()
@@ -250,6 +237,8 @@ def test_time(memgraph_connection):
     result = cursor.fetchall()
     assert result == [(datetime.time(1, 2, 3, 40),)]
 
+
+@pytest.mark.temporal
 def test_date(memgraph_connection):
     conn = memgraph_connection
     cursor = conn.cursor()
@@ -257,15 +246,17 @@ def test_date(memgraph_connection):
     result = cursor.fetchall()
     assert result == [(datetime.date(1994, 7, 12),)]
 
+
+@pytest.mark.temporal
 def test_datetime(memgraph_connection):
     conn = memgraph_connection
     cursor = conn.cursor()
-    cursor.execute(
-        "RETURN $value", {"value": datetime.datetime(2004, 7, 11, 12, 13, 14, 15)}
-    )
+    cursor.execute("RETURN $value", {"value": datetime.datetime(2004, 7, 11, 12, 13, 14, 15)})
     result = cursor.fetchall()
     assert result == [(datetime.datetime(2004, 7, 11, 12, 13, 14, 15),)]
 
+
+@pytest.mark.temporal
 def test_duration(memgraph_connection):
     conn = memgraph_connection
     cursor = conn.cursor()

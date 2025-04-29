8ae59525058faed4402a6d8058ce0b75a437c1e4
matt <mattkjames7@gmail.com>
2025-04-29 17:24:53 +0100

    update flags

--- setup.py
+++ setup.py
@@ -233,7 +233,9 @@ class BuildMgclientExt(build_ext):
             "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
             f'-DCMAKE_C_FLAGS="{self.get_cflags()}"',
             f"-DOPENSSL_USE_STATIC_LIBS={'ON' if self.static_openssl else 'OFF'}",
-            '-DPKG_CONFIG_EXECUTABLE="pkg-config --static"'
+            '-DPKG_CONFIG_EXECUTABLE="pkg-config --static"',
+            '-DCMAKE_SHARED_LINKER_FLAGS="-lcrypt32 -lws2_32 -lgdi32"',
+            '-DCMAKE_EXE_LINKER_FLAGS="-lcrypt32 -lws2_32 -lgdi32"'
         ]
 
         finalize_cmake_config_command = getattr(self, "finalize_cmake_config_command_" + sys.platform, None)
